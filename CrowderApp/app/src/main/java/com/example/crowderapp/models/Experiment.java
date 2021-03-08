5c4b06f641d8b23c99aa2a7299751fc6e219a04f
JDialaUofA <justineb@ualberta.ca>
2021-03-08 15:09:05 -0700

    Added some general functions like getters and setters. Also added some experiment attributes and general framework for experiment object. Related to US 01.02.01

--- CrowderApp/app/src/main/java/com/example/crowderapp/models/Experiment.java
+++ CrowderApp/app/src/main/java/com/example/crowderapp/models/Experiment.java
@@ -1,4 +1,62 @@
 package com.example.crowderapp.models;
 
 public class Experiment {
+
+    private int experimentID;
+    private int minTrials;
+    private boolean isEnded;
+    private boolean isUnpublished;
+    private boolean isLocationRequired;
+    private int ownerID;
+
+    public Experiment() {
+    }
+
+    public int getMinTrials() {
+        return minTrials;
+    }
+
+    public void setMinTrials(int minTrials) {
+        this.minTrials = minTrials;
+    }
+
+    public int getExperimentID() {
+        return experimentID;
+    }
+
+    public boolean isEnded() {
+        return isEnded;
+    }
+
+    public boolean isUnpublished() {
+        return isUnpublished;
+    }
+
+    public boolean isLocationRequired() {
+        return isLocationRequired;
+    }
+
+    public int getOwnerID() {
+        return ownerID;
+    }
+
+    public void setOwnerID(int ownerID) {
+        this.ownerID = ownerID;
+    }
+
+    public void setExperimentID(int experimentID) {
+        this.experimentID = experimentID;
+    }
+
+    public void setEnded(boolean ended) {
+        isEnded = ended;
+    }
+
+    public void setUnpublished(boolean unpublished) {
+        isUnpublished = unpublished;
+    }
+
+    public void setLocationRequired(boolean locationRequired) {
+        isLocationRequired = locationRequired;
+    }
 }
