141f71115bd349049ee5b8c9d263a38cf64a043e
Ryan Sandoval <rsandova@ualberta.ca>
2021-03-15 15:06:32 -0600

    Added User handler and modified user activity to use it

--- CrowderApp/app/src/main/java/com/example/crowderapp/views/ProfileFragment.java
+++ CrowderApp/app/src/main/java/com/example/crowderapp/views/ProfileFragment.java
@@ -1,14 +1,27 @@
 package com.example.crowderapp.views;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
 import androidx.fragment.app.Fragment;
 
 import com.example.crowderapp.R;
+import com.example.crowderapp.controllers.UserHandler;
+import com.example.crowderapp.models.User;
 
 public class ProfileFragment extends Fragment {
+
+    UserHandler userHandler;
+
+    TextView phoneBox;
+    TextView emailBox;
+    TextView usernameBox;
+
+    User user;
 
     public ProfileFragment() {
 
@@ -25,10 +38,24 @@ public class ProfileFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        userHandler = new UserHandler(getActivity().getSharedPreferences(
+                UserHandler.USER_DATA_KEY, Context.MODE_PRIVATE));
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.profile_fragment, container, false);
+        View view = inflater.inflate(R.layout.profile_fragment, container, false);
+
+        emailBox = view.findViewById(R.id.profile_email);
+        phoneBox = view.findViewById(R.id.profile_phone);
+        usernameBox = view.findViewById(R.id.profile_username);
+
+        userHandler.getCurrentUser().addOnSuccessListener(user -> {
+            emailBox.setText(user.getEmail());
+            phoneBox.setText(user.getPhone());
+            usernameBox.setText(user.getUid());
+        });
+
+        return view;
     }
 }
