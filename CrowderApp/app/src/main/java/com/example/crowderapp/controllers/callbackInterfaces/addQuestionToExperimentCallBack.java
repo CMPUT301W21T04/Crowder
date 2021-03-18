12cd10cdf37b59419cb66cd724b397cf273cf600
JDialaUofA <justineb@ualberta.ca>
2021-03-18 15:42:01 -0600

    Added comment handler with unit tests

---
+++ CrowderApp/app/src/main/java/com/example/crowderapp/controllers/callbackInterfaces/addQuestionToExperimentCallBack.java
@@ -0,0 +1,9 @@
+package com.example.crowderapp.controllers.callbackInterfaces;
+
+import com.example.crowderapp.models.posts.Question;
+
+import java.util.List;
+
+public interface addQuestionToExperimentCallBack {
+    public void callBackResult(String questionID);
+}
