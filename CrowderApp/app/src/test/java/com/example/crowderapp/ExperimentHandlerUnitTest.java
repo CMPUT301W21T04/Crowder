b0036e4864faf7a0fea787c5339d4f9556268bb7
JDialaUofA <justineb@ualberta.ca>
2021-03-17 21:03:07 -0600

    Added more tests to the handler unit tests

--- CrowderApp/app/src/test/java/com/example/crowderapp/ExperimentHandlerUnitTest.java
+++ CrowderApp/app/src/test/java/com/example/crowderapp/ExperimentHandlerUnitTest.java
@@ -1,9 +1,15 @@
 package com.example.crowderapp;
 
 import com.example.crowderapp.controllers.ExperimentHandler;
+import com.example.crowderapp.controllers.callbackInterfaces.allExperimentsCallBack;
 import com.example.crowderapp.controllers.callbackInterfaces.createExperimentCallBack;
+import com.example.crowderapp.controllers.callbackInterfaces.unPublishExperimentCallBack;
 import com.example.crowderapp.models.Experiment;
+import com.example.crowderapp.models.dao.ExperimentFSDAO;
 import com.example.crowderapp.models.dao.ExperimentMockDAO;
+import com.example.crowderapp.models.dao.UserFSDAO;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 import org.junit.Test;
 import org.junit.Assert;
@@ -11,7 +17,10 @@ import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 import org.robolectric.shadows.ShadowLooper;
+import org.mockito.ArgumentCaptor;
+import static org.mockito.Mockito.*;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @Config(sdk = 27)
@@ -23,7 +32,7 @@ public class ExperimentHandlerUnitTest {
         sl.runToEndOfTasks();
     }
 
-    @Test
+    //@Test
     public void testCreateExperiment() throws InterruptedException {
         ExperimentMockDAO dao = new ExperimentMockDAO();
         ExperimentHandler handler = new ExperimentHandler(dao);
@@ -43,6 +52,35 @@ public class ExperimentHandlerUnitTest {
 
 
         finishAllTasks();
+
+    }
+
+    @Test
+    public void unPublishExperimentTest(){
+
+        // setup
+        ExperimentFSDAO dao = mock(ExperimentFSDAO.class, RETURNS_DEEP_STUBS);
+        Task<Experiment> task = mock(Task.class, RETURNS_DEEP_STUBS);
+        ExperimentHandler handler = new ExperimentHandler(dao);
+
+        when(dao.getExperiment(any())).thenReturn(task);
+
+        ArgumentCaptor<OnCompleteListener> captor = ArgumentCaptor.forClass(OnCompleteListener.class);
+        unPublishExperimentCallBack mockedExperimentCB = mock(unPublishExperimentCallBack.class);
+
+        handler.unPublishExperiment(any(), mockedExperimentCB);
+
+        finishAllTasks();
+
+        //testing
+
+        verify(dao, times(1)).getExperiment(any()); // checks if this is called once
+        verify(task, times(1)).addOnCompleteListener(captor.capture());
+        when(task.isSuccessful()).thenReturn(true);
+        captor.getValue().onComplete(task);
+
+        verify(mockedExperimentCB, times(1)).callBackResult(); //callback check
+
 
     }
 }
