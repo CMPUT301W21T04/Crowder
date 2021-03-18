cae80f62f9d37cd0084e8ce90a571abf0671ca81
MaxiumMeda <43559318+MaxiumMeda@users.noreply.github.com>
2021-03-18 09:45:03 -0600

    Actually finished the create experiment function

--- CrowderApp/app/src/androidTest/java/com/example/crowderapp/UiTestHelperFunctions.java
+++ CrowderApp/app/src/androidTest/java/com/example/crowderapp/UiTestHelperFunctions.java
@@ -1,7 +1,9 @@
 package com.example.crowderapp;
 
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Switch;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.robotium.solo.Solo;
@@ -15,19 +17,38 @@ public class UiTestHelperFunctions {
         MEASUREMENT
     }
 
-    public static void createExperiment(Solo solo, String expName, expTypes type) {
+    public static void createExperiment(Solo solo, String expName, int minTrialAmt, expTypes type) {
         FloatingActionButton addExpButton = (FloatingActionButton) solo.getView(R.id.add_experiment_button);
         EditText expNameText = (EditText) solo.getView(R.id.experiment_name_EditText);
-        // do we need to test for different cases of this?
         Spinner dropDown = (Spinner) solo.getView(R.id.dropdown);
         EditText minTrials = (EditText) solo.getView(R.id.min_trials_EditText);
 
         solo.clickOnView(addExpButton);
         solo.enterText(expNameText, expName);
         solo.clickOnView(dropDown);
+
+        switch (type) {
+            case COUNT:
+                solo.clickOnText("Count");
+                break;
+            case BINOMIAL:
+                solo.clickOnText("Binomial");
+                break;
+            case TALLY:
+                solo.clickOnText("Non-Negative Integer");
+                break;
+            case MEASUREMENT:
+                solo.clickOnText("Measurement");
+                break;
+            default:
+                solo.clickOnText("Count");
+        }
+
+        solo.enterText(minTrials, String.valueOf(minTrialAmt));
+        solo.clickOnText("OK");
     }
 
-    public static void createExperiment(Solo solo, String expName) {
-        createExperiment(solo, expName, expTypes.COUNT);
+    public static void createExperiment(Solo solo, String expName, int minTrialAmt) {
+        createExperiment(solo, expName, minTrialAmt, expTypes.COUNT);
     }
 }
