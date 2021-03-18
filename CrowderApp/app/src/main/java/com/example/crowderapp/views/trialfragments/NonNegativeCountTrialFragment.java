0e6458bb6a8e92465aeec959f5222955df50ae0a
Raynard Dizon <rdizon@ualberta.ca>
2021-03-17 22:50:32 -0600

    Finished hooking up trial fragment UI

--- CrowderApp/app/src/main/java/com/example/crowderapp/views/trialfragments/NonNegativeCountTrialFragment.java
+++ CrowderApp/app/src/main/java/com/example/crowderapp/views/trialfragments/NonNegativeCountTrialFragment.java
@@ -7,6 +7,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,11 +17,40 @@ import androidx.fragment.app.Fragment;
 
 import com.example.crowderapp.R;
 import com.example.crowderapp.controllers.ExperimentHandler;
+import com.example.crowderapp.controllers.callbackInterfaces.addTrialCallBack;
 import com.example.crowderapp.controllers.callbackInterfaces.unPublishExperimentCallBack;
+import com.example.crowderapp.models.CounterTrial;
 import com.example.crowderapp.models.Experiment;
+import com.example.crowderapp.models.Location;
+import com.example.crowderapp.models.TallyExperiment;
+import com.example.crowderapp.models.TallyTrial;
+import com.example.crowderapp.models.Trial;
+import com.example.crowderapp.models.User;
 import com.example.crowderapp.views.AllExperimentsFragment;
 
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
 public class NonNegativeCountTrialFragment extends TrialFragment {
+    TallyExperiment tallyExperiment;
+
+    TextView numCountTextView;
+    TextView aveCountTextView;
+    EditText integerValueEditText;
+    String integerValueString;
+    Button enterButton;
+    Button saveButton;
+
+    private static DecimalFormat df = new DecimalFormat("0.00");
+
+    int numCounts;
+    int currentCount;
+    double average;
+    String averageString;
+
+    private List<TallyTrial> trials = new ArrayList<>();
+
 
 
     public NonNegativeCountTrialFragment() {
@@ -41,5 +73,60 @@ public class NonNegativeCountTrialFragment extends TrialFragment {
         super.onViewCreated(view, savedInstanceState);
         Bundle bundle = getArguments();
         experiment = (Experiment) bundle.getSerializable("Experiment");
+        tallyExperiment = (TallyExperiment) experiment;
+        user = (User) bundle.getSerializable("User");
+
+        numCountTextView = view.findViewById(R.id.num_non_neg_value_textView);
+        aveCountTextView = view.findViewById(R.id.ave_non_neg_value_textView);
+        integerValueEditText = view.findViewById(R.id.non_neg_value_editText);
+        enterButton = view.findViewById(R.id.non_neg_button_enter);
+        saveButton = view.findViewById(R.id.non_neg_button_save);
+
+        enterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numCounts++;
+                numCountTextView.setText(String.valueOf(numCounts));
+                integerValueString = integerValueEditText.getText().toString();
+                currentCount = Integer.valueOf(integerValueString);
+                calculateAverage();
+                tallyExperiment.addNonNegativeCount(currentCount, user.getUid(), new Location());
+                aveCountTextView.setText(averageString);
+                integerValueEditText.setText("");
+            }
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handler.updateExperiment(tallyExperiment);
+                for(Trial trial : trials) {
+                    Log.v(String.valueOf(trial.getExperimentID()), "Trial experiment id");
+                    handler.addTrial(trial, new addTrialCallBack() {
+                        @Override
+                        public void callBackResult(String trialID) {
+                            Log.v(trialID, "Trial ID returned");
+                        }
+                    });
+                }
+                trials = new ArrayList<>();
+                integerValueString = "";
+                numCounts = 0;
+                currentCount = 0;
+                average = 0;
+                averageString = "";
+
+                numCountTextView.setText("0");
+                aveCountTextView.setText("0");
+            }
+        });
+
+
     }
+
+    private void calculateAverage() {
+        average = (average*numCounts+(double)currentCount)/(double)numCounts;
+        averageString = df.format(average);
+    }
+
 }
