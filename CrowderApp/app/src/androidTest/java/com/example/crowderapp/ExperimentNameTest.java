16eb339a51b61b9db331e8e3043aee8187f4f704
dfloren <daniel_florendo1@yahoo.com>
2021-04-09 07:44:45 -0600

    Testing experiment name in trial frag

---
+++ CrowderApp/app/src/androidTest/java/com/example/crowderapp/ExperimentNameTest.java
@@ -0,0 +1,52 @@
+package com.example.crowderapp;
+
+import android.widget.EditText;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import com.robotium.solo.Solo;
+
+import org.junit.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+public class ExperimentNameTest {
+    private Solo solo;
+    String expName = "MyTestExp";
+
+
+    @Rule
+    public ActivityTestRule<MainActivity> testRule = new ActivityTestRule<>(MainActivity.class,
+            true, true);
+
+
+    @Before
+    public void setUp() throws Exception{
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(), testRule.getActivity());
+    }
+
+
+    @Test
+    public void testExpName() {
+        solo.assertCurrentActivity("Not in Main Activity.", MainActivity.class);
+
+        UiTestHelperFunctions.createExperiment(solo, expName, 1);
+        solo.sleep(1000);
+        UiTestHelperFunctions.goToMyExperiments(solo);
+        solo.clickOnText(expName);
+        Assert.assertTrue(solo.searchText(expName));
+
+
+        // Clean up
+        UiTestHelperFunctions.goToMyExperiments(solo);
+        solo.clickOnText(expName);
+        UiTestHelperFunctions.unpublishExp(solo);
+        solo.sleep(1000);
+        UiTestHelperFunctions.goToMyExperiments(solo);
+        solo.sleep(1000);
+
+    }
+}
