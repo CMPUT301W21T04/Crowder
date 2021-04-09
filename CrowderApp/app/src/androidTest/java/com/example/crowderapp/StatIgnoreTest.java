b0729f60cf8796272056d109c61bb942493d9882
MaxiumMeda <43559318+MaxiumMeda@users.noreply.github.com>
2021-04-09 13:39:33 -0600

    test for filter

---
+++ CrowderApp/app/src/androidTest/java/com/example/crowderapp/StatIgnoreTest.java
@@ -0,0 +1,55 @@
+package com.example.crowderapp;
+
+import android.widget.Button;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.robotium.solo.Solo;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class StatIgnoreTest {
+    private Solo solo;
+    String expname = "End Test.__UI.TEST__";
+
+    @Rule
+    public ActivityTestRule<MainActivity> testRule = new ActivityTestRule<>(MainActivity.class, true, true);
+
+    @Before
+    public void setUp() throws Exception{
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(), testRule.getActivity());
+    }
+
+    @Test
+    public void testIgnoreData() {
+        UiTestHelperFunctions.initExp(solo, expname, UiTestHelperFunctions.expTypes.COUNT);
+
+        Button count = (Button) solo.getView(R.id.count_button);
+
+        solo.clickOnView(count);
+
+        Button save = (Button) solo.getView(R.id.count_save_Button);
+
+        solo.clickOnView(count);
+
+        solo.sleep(1000);
+
+        UiTestHelperFunctions.ignoreSelfData(solo);
+
+        UiTestHelperFunctions.openStats(solo);
+
+        Assert.assertFalse(solo.searchText("1"));
+
+        solo.goBack();
+
+        UiTestHelperFunctions.unpublishExp(solo);
+    }
+}
--- CrowderApp/app/src/androidTest/java/com/example/crowderapp/UiTestHelperFunctions.java
+++ CrowderApp/app/src/androidTest/java/com/example/crowderapp/UiTestHelperFunctions.java
@@ -171,4 +171,20 @@ public class UiTestHelperFunctions {
         solo.sleep(1500);
         solo.clickOnText("Location Heatmap");
     }
+
+    public static void openStats(Solo solo) {
+        View dropdown = solo.getView(R.id.more_item);
+        solo.clickOnView(dropdown);
+        solo.sleep(1500);
+        solo.clickOnText("Statistics");
+    }
+
+    public static void ignoreSelfData(Solo solo) {
+        View dropdown = solo.getView(R.id.more_item);
+        solo.clickOnView(dropdown);
+        solo.sleep(1500);
+        solo.clickOnText("User Filter");
+        solo.clickOnCheckBox(0);
+        solo.clickOnView(solo.getView(android.R.id.button1));
+    }
 }
